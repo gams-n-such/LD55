15dca21021507d3d59e9747b54beb7026767fb00
MichalBiesek <michal.biesek@itsilesia.com>
2024-04-14 10:42:15 +0200

    autobattle
    autobattle

--- Demon_assets/Demon.gd
+++ Demon_assets/Demon.gd
@@ -1,16 +1,24 @@
 extends CharacterBody3D
 class_name Demon
 
-var Health := randf_range(100.0, 500.0)
+var MaxHealth := randf_range(100.0, 500.0)
+var Health=MaxHealth
 var Damage := randf_range(10.0, 50.0)
 var Name := "name " + str(randi_range(0, 2048))
 
-@export var SPEED = 15.0
+@export var SPEED = 25.0
 var GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
 var home_pos:Vector3
 var attack=true
 var acctive=false
+var target:Person
 
+func upadate_taget():
+	if $"../../Enemies".get_child_count()>0:
+		target=$"../../Enemies".get_child(randi()%$"../../Enemies".get_child_count())
+	else:
+		get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
+		target=null
 func _ready():
 	call_deferred("actor_setup")
 	actor_setup()
@@ -26,17 +34,20 @@ func actor_setup():
 	#		$"../goal".global_position,
 	#		$"../goal".global_position+Vector3.DOWN*100.0
 	#		))
-	set_movement_target($"../../goal".global_position)
+	upadate_taget()
+	set_movement_target(target.global_position)
 	home_pos=global_position
 			
 	print("SET",NavigationServer3D.map_get_closest_point_to_segment(
 			get_world_3d().navigation_map,
-			$"../../goal".global_position,
-			$"../../goal".global_position+Vector3.DOWN*100.0
+			target.global_position,
+			target.global_position+Vector3.DOWN*100.0
 			))
 
 
 func _physics_process(delta):
+	$hpBar.hp=Health
+	$hpBar.max_hp=MaxHealth
 	if acctive:
 		# movement
 		var input_dir = Vector2.ZERO#Input.get_vector("left", "right", "forward", "backward")
@@ -55,9 +66,13 @@ func _physics_process(delta):
 			if attack==true:
 				attack=false
 				set_movement_target(home_pos)
+				if is_instance_valid(target):
+					target.damage(50)
+					self.damage(50)
 			else:
 				attack=true
-				set_movement_target($"../../goal".global_position)
+				upadate_taget()
+				set_movement_target(target.global_position)
 			return
 		
 		#set_movement_target($"../goal".global_position)
@@ -73,11 +88,11 @@ func set_movement_target(movement_target: Vector3):
 	$NavigationAgent3D.set_target_position(movement_target)
 
 
-	
 
-	
-	
-	
+func damage(v): 
+	Health-=v
+	if Health<=0:
+		self.queue_free()
 
 func move_in_direction(input_dir : Vector2, delta : float):
 	if not is_on_floor():
