6a44fdddbc1f57352dbeac2a8ca580cd26f8e91a
Denis Maximenko <maximenko@owlcat.games>
2024-04-14 10:00:50 +0400

    Add name pool for the demons

--- Demon_assets/demon_type.gd
+++ Demon_assets/demon_type.gd
@@ -6,3 +6,5 @@ class_name DemonType extends Resource
 @export var attack_speed : float = 1.0
 @export var health : float = 10.0
 @export var move_speed : float = 10.0
+
+@export var possible_names : Array[String] = [ "NoName" ]
