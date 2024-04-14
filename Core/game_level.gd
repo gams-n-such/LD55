deda47a02cd4cde69cbde3e9d519cbce82e4033a
Denis Maximenko <manlok71@gmail.com>
2024-04-14 17:20:22 +0400

    Implement enemy spawn settings in game level resource

--- Core/game_level.gd
+++ Core/game_level.gd
@@ -2,7 +2,7 @@ class_name GameLevel extends Resource
 
 @export_category("Level")
 @export var location_name : String = "Some Town"
-@export var demon_count : int = 0
+@export var demon_count : int = 5
 @export var demon_pool : DemonPool
-@export var enemies : Array[Resource] = []
+@export var enemies : Array[EnemyGroup] = []
 
