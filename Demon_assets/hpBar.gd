59e3f72a0ae7f3496cabae83b7b514d3057cbbcd
Denis Maximenko <maximenko@owlcat.games>
2024-04-15 16:50:23 +0400

    Silence unused delta warnings

--- Demon_assets/hpBar.gd
+++ Demon_assets/hpBar.gd
@@ -3,7 +3,7 @@ extends MeshInstance3D
 var hp=5.0
 var max_hp=10.0
 
-func _process(delta):
+func _process(_delta):
 	var value=float(hp)/float(max_hp)
 	$green.position.x=-(1-value)*0.5
 	$green.scale.x=value*1.01
--- Demon_assets/legion/card/legion_card.gd
+++ Demon_assets/legion/card/legion_card.gd
@@ -54,6 +54,6 @@ func disconnect_signals():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
