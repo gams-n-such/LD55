87670b6796c5a4c54849dfff2418f02f3d75dc5a
Denis Maximenko <maximenko@owlcat.games>
2024-04-15 18:14:52 +0400

    Game flow routing WIP

--- Core/game.gd
+++ Core/game.gd
@@ -10,11 +10,9 @@ extends Node
 @onready var arena_scene : PackedScene = load("res://Battler/battle_arena.tscn")
 
 var current_level : GameLevel
+var current_campaign : Campaign = preload("res://Levels/main_campaign.tres")
 
 func _ready():
-	pass # Replace with function body.
-
-func _process(_delta):
 	pass
 
 func start_new_game(with_tutorial : bool):
@@ -23,7 +21,33 @@ func start_new_game(with_tutorial : bool):
 		# TODO
 		get_tree().change_scene_to_packed(tinder_scene)
 	else:
-		get_tree().change_scene_to_packed(tinder_scene)
+		start_campaign()
+
+func start_campaign():
+	assert(not current_campaign.levels.is_empty())
+	start_level(0)
+
+func move_to_previous_level():
+	var cur_level_idx = current_campaign.levels.find(current_level)
+	var prev_level_idx = cur_level_idx - 1
+	start_level(prev_level_idx)
+
+func move_to_next_level():
+	var cur_level_idx = current_campaign.levels.find(current_level)
+	var next_level_idx = cur_level_idx + 1
+	start_level(next_level_idx)
+
+func start_level(level_idx : int):
+	if level_idx >= current_campaign.levels.size():
+		win_game()
+		return
+	if level_idx < 0:
+		level_idx = 0
+	current_level = current_campaign.levels[level_idx]
+	get_tree().change_scene_to_packed(tinder_scene)
+
+func win_game():
+	pass # TODO
 
 func switch_to_menu_level():
 	get_tree().change_scene_to_packed(menu_scene)
