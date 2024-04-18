3036efc657756d6fd1c4deefbff33355fdf5d5af
Denis Maximenko <maximenko@owlcat.games>
2024-04-18 10:41:22 +0400

    Remove demon management from player state

--- Core/player_state.gd
+++ Core/player_state.gd
@@ -1,32 +1,10 @@
 class_name PlayerState extends Node
 
-@export var player_name : String = ""
-@export var player_legion : Legion = Legion.new()
-@export var player_liked : Array[DemonInstance] = []
-
-var legion : Node:
-	get:
-		return %Legion
-
-var demons : Array[DemonInstance]:
-	get:
-		var result: Array[DemonInstance]
-		result.assign(legion.get_children())
-		return result
+@export var player_name : String = "Abobus"
 
 func _ready():
 	pass # Replace with function body.
 
-
-func _process(_delta):
-	pass
-
 func reset():
-	
+	player_name = "Abobus"
 	pass
-
-func add_demon(demon : DemonInstance):
-	player_liked.append(demon)
-
-func remove_demon(demon : DemonInstance):
-	player_liked.erase(demon)
